1096e2aae4a00da983d7243ade275369b24781f4
JohnJeong123 <frenchofkiss@gmail.com>
2019-11-21 16:10:28 +0900

    fixed Config en Ver

--- config/popbill.php
+++ config/popbill.php
@@ -2,19 +2,18 @@
 
 return [
 
-    // 파트너 신청시 발급받은 링크아이디
+    // LinkID
     'LinkID' => 'TESTER',
 
-    // 파트너 신청시 발급받은 비밀키
+    // SecreKey
     'SecretKey' => 'SwWxqU+0TErBXy/9TVjIPEnI0VTUMMSQZtJf3Ed8q3I=',
 
-    // 통신방식 기본은 CURL , PHP curl 모듈 사용에 문제가 있을 경우 STREAM 기재가능.
-    // STREAM 사용시에는 php.ini의 allow_url_fopen = on 으로 설정해야함.
+    // Linkhub SDK HTTP Connection Mode {'CURL' or 'STREAM'}
     'LINKHUB_COMM_MODE' => 'CURL',
 
-    // 연동환경 설정값, 개발용(true), 상업용(false)
+    // Test Mode Configuration true(Test-Mode), false(Real-Mode)
     'IsTest' => true,
 
-    // 인증토큰의 IP제한기능 사용여부, 권장(true)
+    // Authorized Token IPRestrict On/Off Option, Recommand(true)
     'IPRestrictOnOff' => true,
 ];
